268f9c5aa51da8e2811b0d6121a9efd9d5096cca
Abdelmassih <220200503@fa-hists.edu.eg>
2024-04-21 00:20:44 +0200

    ads

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ dotenv.config({path : './config.env'})
 const app = require('./app')
 
 
-<<<<<<< HEAD
 const port = process.env.PORT || 3000;
-=======
-const port = process.env.PORT || 8000;
->>>>>>> 49baad980be8db8cdb9df8fce9158e04829da182
 app.listen(port, () => {
     console.log(`App is running on port ${port}...`)
 })
