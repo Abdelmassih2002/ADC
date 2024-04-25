7bd6b7bbde3650e44ba41883ae987c7ee7dbc693
Abdelmassih Ramsis <114319155+Abdelmassih2002@users.noreply.github.com>
2024-04-25 04:12:53 +0200

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -223,9 +223,14 @@ exports.userInfo = catchAsync(async (req, res, next) => {
 });
 
 exports.updateInfo = catchAsync(async (req, res, next) => {
-  const { firstName, lastName, gender, phoneNumber, weight, height } = req.body;
+  const { firstName, lastName, gender, email, phoneNumber, weight, height } = req.body;
   const token = req.body.token;
   console.log(token);
+  if (!(firstName && lastName && gender && email && phoneNumber && weight && height)) {
+    return res.status(400).json({
+      error: "All fields must be filled",
+    });
+  }
   try {
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     const userEmail = decodedToken.data[0];
@@ -235,11 +240,13 @@ exports.updateInfo = catchAsync(async (req, res, next) => {
       firstName,
       lastName,
       gender,
+      email,
       phoneNumber,
       weight,
       height,
     });
-    res.send("User info updated successfully");
+    // Send response with updated user info and token
+    res.send("User info updateddd successfully");
     createSendToken(updatedData, 200, req, res);
   } catch (error) {
     console.error("Error updating update info:", error);
@@ -248,3 +255,30 @@ exports.updateInfo = catchAsync(async (req, res, next) => {
     });
   }
 });
+
+// exports.updateInfo = catchAsync(async (req, res, next) => {
+//   const { firstName, lastName, gender, phoneNumber, weight, height } = req.body;
+//   const token = req.body.token;
+//   console.log(token);
+//   try {
+//     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+//     const userEmail = decodedToken.data[0];
+//     const updatedData = decodedToken.data;
+//     console.log(updatedData);
+//     await db.collection("users").doc(userEmail).update({
+//       firstName,
+//       lastName,
+//       gender,
+//       phoneNumber,
+//       weight,
+//       height,
+//     });
+//     res.send("User info updated successfully");
+//     createSendToken(updatedData, 200, req, res);
+//   } catch (error) {
+//     console.error("Error updating update info:", error);
+//     return res.status(500).json({
+//       error: "Internal Server Error",
+//     });
+//   }
+// });
