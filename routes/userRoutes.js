daa4ff79577aa30eca1e584af00df80e5fd1ad11
Amr Ismail <96202346+amrismail123@users.noreply.github.com>
2024-04-28 21:41:20 +0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,13 +3,14 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 
-router.patch('/userinfo' ,authController.userInfo)
-router.patch('/updateinfo' ,authController.updateInfo)
 router.post('/signup', authController.signup);
 router.post('/login', authController.login)
 router.get('/logout', authController.logout)
 router.post('/forgotpassword', authController.forgotPassword)
 router.patch('/resetpassword/:token', authController.resetPassword)
+router.patch('/userinfo' ,authController.userInfo)
+router.patch('/updateinfo' ,authController.updateInfo)
+router.patch('/diabeticInfo' ,authController.diabeticInfo)
 // router.patch('/updateMyPassword', authController.protect, authController.updatePassword)
 
 
