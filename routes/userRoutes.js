188ae2ca14b18bcfceb93df68a8dd08ff1034052
Abdelmassih Ramsis <114319155+Abdelmassih2002@users.noreply.github.com>
2024-05-26 01:47:31 +0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,16 +2,19 @@ const express = require('express');
 const authController = require('../controllers/authController');
 const router = express.Router();
 
-
+// Existing routes
 router.post('/signup', authController.signup);
-router.post('/login', authController.login)
-router.get('/logout', authController.logout)
-router.post('/forgotpassword', authController.forgotPassword)
-router.patch('/resetpassword/:token', authController.resetPassword)
-router.patch('/userinfo' ,authController.userInfo)
-router.patch('/updateinfo' ,authController.updateInfo)
-router.patch('/diabeticInfo' ,authController.diabeticInfo)
-// router.patch('/updateMyPassword', authController.protect, authController.updatePassword)
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
 
+// New routes for password reset
+router.post('/updatePassword', authController.updatePassword);
+
+router.post('/request-password-reset', authController.requestPasswordReset); // Add this line for requesting password reset
+router.post('/reset-password', authController.resetPassword);
+
+// Existing routes for user info and update info
+router.patch('/userinfo', authController.userInfo);
+router.patch('/updateinfo', authController.updateInfo);
 
 module.exports = router;
